0a1fa0ceea97a93bb41d700b0e5faebbabe1dcae
Bhushan Patel <109128881+Bhushan4-tech@users.noreply.github.com>
2024-08-17 16:00:09 +0530

    setup update

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ setuptools.setup(
     description="A small python package for CNN app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
+    # url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    # project_urls={
+    #     "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    # },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
